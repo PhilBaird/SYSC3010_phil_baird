60c2bc073fc78262e4a3a50ec76dfe045fbcb0eb
PhilBaird <plipmana@gmail.com>
2020-09-27 22:29:28 -0400

    style: renamed files for lab2, cont.

---
+++ Lab2/lab2-hardware-step1.py
@@ -0,0 +1,43 @@
+from sense_emu import SenseHat
+import time
+
+# import keyboard
+
+
+sense = SenseHat()
+
+keydown = 0
+keyup = 0
+p = True
+
+while True:
+    time.sleep(1)
+    sense.clear()
+    # key = keyboard.read_key()
+    # print( key )
+
+    # if key == keydown || key == keyup:
+    if p:
+        for i in range(7):
+            sense.set_pixel(i, 0, 0, 255, 255)
+        for i in range(8):
+            sense.set_pixel(0, i, 0, 255, 255)
+        for i in range(7):
+            sense.set_pixel(i, 3, 0, 255, 255)
+        for i in range(4):
+            sense.set_pixel(7, i, 0, 255, 255)
+    else:
+        for i in range(7):
+            sense.set_pixel(i, 0, 0, 255, 0)
+        for i in range(8):
+            sense.set_pixel(0, i, 0, 255, 0)
+        for i in range(7):
+            sense.set_pixel(i, 3, 0, 255, 0)
+        for i in range(8):
+            sense.set_pixel(7, i, 0, 255, 0)
+        for i in range(7):
+            sense.set_pixel(i, 7, 0, 255, 0)
+    p = not p
+
+
+
