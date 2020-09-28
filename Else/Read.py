bd872be3c11fee28fca078fc6c6cdac9d1e876d5
PhilBaird <plipmana@gmail.com>
2020-09-27 21:19:13 -0500

    feat: added files for lab 2

---
+++ Else/Read.py
@@ -0,0 +1,20 @@
+import requests
+def read():
+    URL='https://api.thingspeak.com/channels/1161282/fields/1.json?api_key='
+    KEY='F2Q2UUQ0HDTLMK25'
+    HEADER='&results=20'
+    NEW_URL=URL+KEY+HEADER
+    try:
+        data=requests.get(NEW_URL).json()
+        #print(data)
+        id = data['channel']['id']
+        field = data['feeds']
+        print(field)
+    except:
+        print("connection failed")
+        
+    
+
+if __name__ == "__main__":
+   
+    read()
---
+++ Else/Temp.py
@@ -0,0 +1,12 @@
+from sense_emu import SenseHat
+
+sense = SenseHat()
+
+while True:
+    temp = sense.get_temperature()
+    
+      if temp >= 20:
+        sense.set_pixels([(255,0 ,0) for i in range(64)])
+    else:
+        sense.set_pixels([(0,0,255) for i in range(64)])
+        
