0d3b5d795c5277a5d2fa2c29872edb34f36bbb70
PhilBaird <plipmana@gmail.com>
2020-09-27 23:08:12 -0400

    fix: code for changing initials with joystick

--- Lab2/lab2-hardware-step1.py
+++ Lab2/lab2-hardware-step1.py
@@ -1,8 +1,4 @@
 from sense_emu import SenseHat
-import time
-
-# import keyboard
-
 
 sense = SenseHat()
 
@@ -11,33 +7,29 @@ keyup = 0
 p = True
 
 while True:
-    time.sleep(1)
-    sense.clear()
-    # key = keyboard.read_key()
-    # print( key )
-
-    # if key == keydown || key == keyup:
-    if p:
-        for i in range(7):
-            sense.set_pixel(i, 0, 0, 255, 255)
-        for i in range(8):
-            sense.set_pixel(0, i, 0, 255, 255)
-        for i in range(7):
-            sense.set_pixel(i, 3, 0, 255, 255)
-        for i in range(4):
-            sense.set_pixel(7, i, 0, 255, 255)
-    else:
-        for i in range(7):
-            sense.set_pixel(i, 0, 0, 255, 0)
-        for i in range(8):
-            sense.set_pixel(0, i, 0, 255, 0)
-        for i in range(7):
-            sense.set_pixel(i, 3, 0, 255, 0)
-        for i in range(8):
-            sense.set_pixel(7, i, 0, 255, 0)
-        for i in range(7):
-            sense.set_pixel(i, 7, 0, 255, 0)
-    p = not p
+    for event in sense.stick.get_events():
+        if event.action == 'pressed' & (event.direction == 'up' | event.direction == 'down' | event.direction == 'left' | event.direction == 'right'):
+            if p:
+                for i in range(7):
+                    sense.set_pixel(i, 0, 0, 255, 255)
+                for i in range(8):
+                    sense.set_pixel(0, i, 0, 255, 255)
+                for i in range(7):
+                    sense.set_pixel(i, 3, 0, 255, 255)
+                for i in range(4):
+                    sense.set_pixel(7, i, 0, 255, 255)
+            else:
+                for i in range(7):
+                    sense.set_pixel(i, 0, 0, 255, 0)
+                for i in range(8):
+                    sense.set_pixel(0, i, 0, 255, 0)
+                for i in range(7):
+                    sense.set_pixel(i, 3, 0, 255, 0)
+                for i in range(8):
+                    sense.set_pixel(7, i, 0, 255, 0)
+                for i in range(7):
+                    sense.set_pixel(i, 7, 0, 255, 0)
+            p = not p
 
 
 
