e2193789aea6226d060ac596d6c4c66390cffe45
PhilBaird <plipmana@gmail.com>
2020-10-08 08:56:22 -0500

    feat: fixed interview code

--- Lab4/interview.py
+++ Lab4/interview.py
@@ -9,26 +9,29 @@ bool = False
 
 while True:
     for event in sense.stick.get_events():
-        print(event)
-        if event.action == 'pressed' &  event.direction == 'left' :
+        
+        if event.action is 'pressed' and (event.direction is 'up' or event.direction is 'down' or event.direction is 'left' or event.direction is 'right'):
+            print(event, bool)
             if bool:
+                sense.clear()
                 if p:
+                    for i in range(8):
+                        sense.set_pixel(3, i, 0, 255, 255)
                     for i in range(7):
-                        sense.set_pixel(i, 3, 0, 255, 255)
-                    for i in range(7):
-                        sense.set_pixel(i, 7, 0, 255, 255)
+                        sense.set_pixel(i, 0, 0, 255, 255)
                 else:
+                    for i in range(8):
+                        sense.set_pixel(0, i, 0, 255, 0)
                     for i in range(7):
                         sense.set_pixel(i, 0, 0, 255, 0)
                     for i in range(7):
                         sense.set_pixel(i, 3, 0, 255, 0)
                     for i in range(8):
                         sense.set_pixel(7, i, 0, 255, 0)
-                    for i in range(7):
-                        sense.set_pixel(i, 7, 0, 255, 0)
+                    
                 p = not p
-            else:
-                bool = not bool
+            
+            bool = not bool
 
 
 
